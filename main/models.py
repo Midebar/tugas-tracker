8f66e7ac430d6863eb182876a3c88ff8dc0e1c1f
midebar <midebar@gmail.com>
2024-02-19 11:09:30 +0700

    Tugas 2

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Item(models.Model):
-    name = models.CharField()
-    amount = models.IntegerField
-    description = models.TextField
+    name = models.CharField(max_length=150)
+    amount = models.IntegerField()
+    description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main(request):
+    content={
+        "app_name" : "Tugas Tracker",
+        "name": "Mikhael Deo Barli",
+        "class": "A",
+    }
+    return render(request, "main_landingpage.html", {"content":content,})
