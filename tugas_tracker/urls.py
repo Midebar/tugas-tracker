f2a3640b6ac9d5f47f2ac917c47b08356e5296b2
midebar <midebar@gmail.com>
2024-02-19 10:47:51 +0700

    Added models

--- tugas_tracker/urls.py
+++ tugas_tracker/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import urls as main_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/',  main_urls)
 ]
