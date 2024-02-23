5cc29ed0d9f4c735916c7afcf120480e7bee6ebf
Mikhael <midebar@gmail.com>
2024-02-23 21:19:48 +0700

    Finished tugas 4, no readme yet

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from .models import Item
 
 class ItemForm(ModelForm):
     class Meta:
         model = Item
         fields = "__all__"
+
+        widgets = {
+            'deadline':TextInput(attrs={'type':'datetime-local'}),
+        }
